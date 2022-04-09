05d34228a9665f4ab82de30437907fd46192b430
Carles Garcia Bercial <Carles.GarciaBercial@phoenix.edu>
2022-04-08 23:38:52 -0700

    Added MUI5

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../src/theme';
+import createEmotionCache from '../src/createEmotionCache';
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta property="og:title" content="Mentr" key="title" />
+        <title>Mentr</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
 export default MyApp
